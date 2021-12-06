7dea5b69d2c459afa655b914ab197ec870d37d2c
Pietro Morichetti <49244593+wilsonjefferson@users.noreply.github.com>
2021-12-06 15:23:32 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from core.UserCommandLineInterface import UserCommandLineInterface
+
+if __name__ == '__main__':
+
+    songs_csv_file = r'./sources/processed_data/spotify_music/SpotifyFeatures.csv'
+    users_csv_file = r'./sources/User.csv'
+    history_csv_file = r'./sources/History.csv'
+    ratings_csv_file = r'./sources/processed_data/yahoo_music/Ratings.csv'
+
+    cli = UserCommandLineInterface(songs_csv_file, users_csv_file, history_csv_file, ratings_csv_file)
+    login_state = cli.login_page()
+
+    while login_state is True:
+        login_state = cli.home_page()
